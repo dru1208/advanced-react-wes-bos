e6f5200c72a57f247daa291beae299293441a44b
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-10-15 02:16:47 +0000

    practice with inject global, nav bar links added, and more css

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header.js'
 import Meta from './Meta.js'
-import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 // using React context API with theme provider, you don't have to pass down the object theme to each child, they can just access it
 
@@ -15,6 +15,8 @@ const theme = {
   bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
 };
 
+theme.lightGrey = theme.lightgrey
+
 const StyledPage = styled.div`
   background: white;
   color: ${props => props.theme.black};
@@ -26,6 +28,35 @@ const Inner = styled.div`
   padding: 2rem;
 `;
 
+// this is used to set global CSS styles that are not scoped to individual components, as it is global the html tag must be specified
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2');
+    format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+    font-family: 'radnika_next'
+  }
+  *, *.:before, *:after {
+    box-sizing: inherit
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+  a{
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+// note, can't use props theme because inject global isn't inside the themeprovider
 
 class Page extends Component {
   render() {
